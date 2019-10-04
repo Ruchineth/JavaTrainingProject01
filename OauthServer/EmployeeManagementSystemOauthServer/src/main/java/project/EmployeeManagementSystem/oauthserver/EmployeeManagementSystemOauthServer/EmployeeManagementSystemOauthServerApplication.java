4020dcb08b4a45779bf6e2a52de7f2b985e51e77
SacheeSubasingha <sacheesubasingha9@gmail.com>
2019-10-04 15:21:32 +0530

    2019-10-04-StartingPoint

---
+++ OauthServer/EmployeeManagementSystemOauthServer/src/main/java/project/EmployeeManagementSystem/oauthserver/EmployeeManagementSystemOauthServer/EmployeeManagementSystemOauthServerApplication.java
@@ -0,0 +1,13 @@
+package project.EmployeeManagementSystem.oauthserver.EmployeeManagementSystemOauthServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemOauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemOauthServerApplication.class, args);
+	}
+
+}
---
+++ UserInterface/EmployeeManagementSystemUI/src/main/java/project/EmployeeManagementSystem/ui/EmployeeManagementSystemUI/EmployeeManagementSystemUiApplication.java
@@ -0,0 +1,13 @@
+package project.EmployeeManagementSystem.ui.EmployeeManagementSystemUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemUiApplication.class, args);
+	}
+
+}
